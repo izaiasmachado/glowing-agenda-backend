6a4c5164d54a7c5951b31028f1f1ddc26627a53e
Izaias Machado <izaiasmachado.dev@gmail.com>
2024-09-21 10:02:24 -0300

    feat: parametrize database connection

--- src/lib/environment.js
+++ src/lib/environment.js
@@ -7,6 +7,8 @@ const environmentSchema = zod.object({
   AGENDAMENTOS_NODE_ENV: zod.string().default("development"),
   AGENDAMENTOS_SMTP_USER: zod.string(),
   AGENDAMENTOS_SMTP_PASSWORD: zod.string(),
+  AGENDAMENTOS_MONGODB_URI: zod.string(),
+  AGENDAMENTOS_MONGODB_DB_NAME: zod.string().default(`glowing-agenda`),
 });
 
 const environment = environmentSchema.parse(process.env);
--- src/lib/mongoose.js
+++ src/lib/mongoose.js
@@ -1,8 +1,12 @@
 const mongoose = require("mongoose");
+const {
+  AGENDAMENTOS_MONGODB_URI,
+  AGENDAMENTOS_MONGODB_DB_NAME,
+} = require("./environment");
 
 mongoose
-  .connect("mongodb://root:root@0.0.0.0:27017", {
-    dbName: "test",
+  .connect(AGENDAMENTOS_MONGODB_URI, {
+    dbName: AGENDAMENTOS_MONGODB_DB_NAME,
   })
   .then(() => console.log("Connected!!"))
   .catch((err) => console.error("Error connecting to mongo", err));
